f347e8e314b95de108405f3b28614c7f5be8ab66
Alfchee <alfchee@Mac-Alf.local>
2014-08-01 23:34:32 -0600

    First commit of the index and the stylesheet

---
+++ js/vendor/main.js
@@ -0,0 +1,9 @@
+
+$(document).on('ready', function() {
+    $('.flexslider').flexslider({
+                animation: "slide",
+                animationSpeed: 3000,
+                slideshowSpeed: 7000
+              });
+});
+
